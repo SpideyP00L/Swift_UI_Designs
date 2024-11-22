9c6ec3ed80ee3ad14d4a3ae42120b5366d51afc7
Jeet Panchal <jeetbpanchal@gmail.com>
2024-11-22 16:21:58 -0500

    Initial Commit

---
+++ SwiftUIDesigns/SwiftUIDesignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignsApp.swift
+//  SwiftUIDesigns
+//
+//  Created by Jeet Panchal on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
